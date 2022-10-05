96d8248200122d63daf58f2f2c23311e72f323b5
khovisfersa <lucaskfsantos@gmail.com>
2022-10-05 01:07:54 -0300

    file-upload  functional

--- src/respostas.js
+++ src/respostas.js
@@ -7,6 +7,7 @@ const process = require('process');
 let formidable = require('formidable');
 const cors = require('cors');
 const {  Blob } = require( 'buffer')
+const path = require('path')
 
 
 router.use(cors({
@@ -24,7 +25,8 @@ const pool = new Pool({
 })
 
 
-const base = '/home/admin/tcc/fileSystem/'
+// const base = '/home/admin/tcc/fileSystem/'
+const base = path.join(__dirname, '..', 'fileSystem','tmp')
 
 /////////////////rotas\\\\\\\\\\\\\\\\\\
 
@@ -62,29 +64,65 @@ router.post('/uploadaudio', (req,res) => {
 	})
 })
 
-
 router.post('/upload_audio', async (req,res) => {
-	try {
-		console.log("veio pra ca")
-		
-		var filename = req.body.filename
-		console.log("req.body: ")
-		console.log("req.filename: " + req.filename)
-		console.log("////////////////////////////")
 
-		console.log("file name: " + filename)
-		const  blob = new Blob(req.body.file, {
-			type: 'audio/mp3'
+		let form = new formidable.IncomingForm();
+
+		console.log()
+		form.parse(req, (error, fields, file) => {
+			let file_path = file.file.filepath
+			let new_path = path.join(base, fields.filename + '.mp3')
+			try {
+				fs.access(file_path, fs.constants.F_OK,(err) => {
+					if(err) throw err
+					console.log("existe!")
+				})
+				fs.chmod(file_path, 0o777, (err) => {
+					if (err) throw err
+					console.log("deu bom, eu acho")
+				})
+				fs.rename(file_path, new_path	, (err) => {
+					if(err) throw err;
+					console.log("deu bom, meu consagrado")
+					return res.status(200).send("Agora sim, deu bom")
+				})
+			} catch(err) {
+				console.log("deu ruim, menó")
+				return res.status(400).send("deu ruim, menó")
+			} 
 		})
-
-		const buffer = Buffer.from( await blob.arrayBuffer() )
-		fs.writeFile(base + 'tmp/' + filename + '.mp3', buffer, () => console.log('audio saved!') );
-		return res.status(200).send("file created successfully")
-	} catch(err) {
-		console.log(err)
-		return res.status(400).send(err)
-	}
 })
+
+
+// router.post('/upload_audio', async (req,res) => {
+// 	try {
+// 		console.log("veio pra ca")
+		
+// 		var filename = req.body.filename || "teste"
+// 		console.log("req.body: ")
+// 		console.log("req.filename: " + req.filename)
+// 		console.log("////////////////////////////")
+
+// 		console.log("file name: " + filename)
+
+// 		var file_path = base + filename + ".mp3"
+
+
+
+// 		const  blob = new Blob(req.body.file, {
+// 			type: 'audio/mp3'
+// 		})
+
+// 		fs.createWriteStream(file_path).write(JSON.stringify(blob))
+
+// 		// const buffer = Buffer.from( await blob.arrayBuffer() )
+// 		// fs.writeFile(base + 'tmp/' + filename + '.mp3', buffer, () => console.log('audio saved!') );
+// 		return res.status(200).send("file created successfully")
+// 	} catch(err) {
+// 		console.log(err)
+// 		return res.status(400).send(err)
+// 	}
+// })
 
 
 router.get('/audio',(req,res) => {
@@ -95,7 +133,7 @@ router.get('/audio',(req,res) => {
     const videoSize = fs.statSync(videoPath).size;
     const chunkSize = 1*1e6;
     const start = Number(range.replace(/\D/g, ""));
-    const end = Math.min(start + chunkSize, videoSize-1);
+    const end = Math.min(start + chunkSize, videoSize - 1);
     const contentLength = end - start + 1
     const headers = {
             "Content-Range": `bytes ${start} - ${end}/${videoSize}`,
